25c7528ded717dd3a4eead9719230861ebdd24c5
DIANA HENAO <osoriohenao1140151@correo.itm.edu.co>
2026-09-19 00:27:33 -0500

    Add services layer, DTOs, AutoMapper and match board (Module 4)

---
+++ SQLQuery3.sql
@@ -0,0 +1,12 @@
+﻿SELECT t.Name, t.City, t.CreatedDate, t.IsActive
+FROM Teams t;
+ 
+SELECT m.Id,
+       ht.Name AS HomeTeam, at.Name AS AwayTeam,
+       m.Date, m.Status,
+       m.HomeOdds, m.DrawOdds, m.AwayOdds,
+       m.CreatedDate
+FROM Matches m
+JOIN Teams ht ON ht.Id = m.HomeTeamId
+JOIN Teams at ON at.Id = m.AwayTeamId
+ORDER BY m.Date;
